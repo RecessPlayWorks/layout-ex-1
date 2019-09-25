3d9adb551dfb048ebe1a4c116d447270a6dadb94
Beau Jackson <beau.html@gmail.com>
2019-09-25 16:27:36 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-// import SearchInput from "./SearchInput";
-// import ImageGallery from "./ImageGallery";
+import ReactGA from 'react-ga';
 import Header from './Header';
 
+
+
+function initializeReactGA() {
+  ReactGA.initialize("UA-148764400-1");
+  ReactGA.pageview('/homepage');
+}
 const App = () => {
   return (
     <div className="app-js-div">
